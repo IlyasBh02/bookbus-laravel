c3fceb4ed2b58f5e82b9f57e5c9dd58ed38df37b
IlyasBh02 <ilyasbahsi9@gmail.com>
2026-02-02 09:32:57 +0100

    edit in the strecture of the project and clean some folder's

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BookBus</title>
+</head>
+<body>
+    <h1>Welcome to BookBus Laravel Project</h1>
+    <p>This is the J1 Livrable setup.</p>
+</body>
+</html>
